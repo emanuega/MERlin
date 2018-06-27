c0de2adc5d2141fd07a52218575d5bf9852cd34e
emanuega <emanuega0@gmail.com>
2018-06-27 09:52:10 -0400

    Initial commits

---
+++ merfish_code/schedule/executor.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+import multiprocessing
+
+from merfish_code.analysis import analysistask
+
+class Executor(object):
+
+    def __init__(self, task):
+        self.task = task
+    
+    @abstractmethod
+    def run(self):
+        pass 
+
+    def is_complete(self):
+        return self.task.is_complete()
+
+
+class LocalExecutor(Executor):
+
+    def __init__(self, task):
+        super().__init__(task)
+
+    def run(self):
+        if isinstance(self.task, analysistask.ParallelAnalysisTask):
+            pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
+            pool.map_async(
+                    self.task.run, range(self.task.fragment_count()))
+            print('done with pool')
+
+        else:
+            process = multiprocessing.Process(target=self.task.run)
+            process.start()
--- test.py
+++ test.py
@@ -1,12 +1,31 @@
+
+class SimpleTask(AnalysisTask):
+    def run(self):
+        for i in range(100):
+            print('running')
+            time.sleep(5)
+        super().run()
+
+class SimpleParallelTask(ParallelAnalysisTask):
+    def fragment_count(self):
+        return 50
+
+    def run_for_fragment(self, fragmentIndex):
+        for i in range(10):
+            print('running ' + str(fragmentIndex))
+            time.sleep(1)
+
+
 from merfish_code.data import dataset
 d = dataset.DataSet('180616_TestData')
 from merfish_code.analysis import analysistask
-s = analysistask.SimpleTask(d)
+s = SimpleTask(d)
 from merfish_code.schedule import executor
 e = executor.LocalExecutor(s)
 
 
 
-s2 = analysistask.SimpleParallelTask(d)
+s2 = SimpleParallelTask(d)
 s2.fragment_count()
 e2 = executor.LocalExecutor(s2)
+e2.run()

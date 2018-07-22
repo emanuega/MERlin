bad107b8aa965de981a04510488c9fe7ec36ec32
emanuega <emanuega0@gmail.com>
2018-07-21 23:57:53 -0400

    Added comments

--- merfish_code/core/analysistask.py
+++ merfish_code/core/analysistask.py
@@ -2,36 +2,97 @@ import os
 from abc import ABC, abstractmethod
 import time
 
-class AnalysisTask(object):
+class AnalysisTask(ABC):
 
-    def __init__(self, dataSet, analysisName=None):
+    '''
+    An abstract class for performing analysis on a DataSet. Subclasses
+    should implement the analysis to perform in the run_analysis() function.
+    '''
+
+    def __init__(self, dataSet, parameters=None, analysisName=None):
+        '''Creates an AnalysisTask object that performs analysis on the
+        specified DataSet.
+
+        Args:
+            dataSet: the DataSet to run analysis on.
+            parameters: a dictionary containing parameters used to run the
+                analysis.
+            analysisName: specifies a unique identifier for this
+                AnalysisTask. If analysisName is not set, the analysis name
+                will default to the name of the class.
+        '''
         self.dataSet = dataSet
-
+        self.parameters = parameters
         if analysisName is None:
             self.analysisName = type(self).__name__
         else:
             self.analysisName = analysisName
 
     def run(self):
+        '''Run this AnalysisTask.
+        
+        Upon completion of the analysis, this function informs the DataSet
+        that analysis is complete.
+        '''
+        self.run_analysis()
         self.dataSet.record_analysis_complete(self)
 
+    @abstractmethod
+    def run_analysis(self):
+        '''Perform the analysis for this AnalysisTask.
+
+        This function should be implemented in all subclasses with the
+        logic to complete the analysis.
+        '''
+        pass
+
+    @abstractmethod
+    def get_estimated_memory(self):
+        '''Get an estimate of how much memory is required for this
+        AnalysisTask.
+
+        Returns:
+            a memory estimate in megabytes.
+        '''
+        pass
+
+    @abstractmethod
+    def get_estimated_time(self):
+        '''Get an estimate for the amount of time required to complete
+        this AnalysisTask.
+
+        Returns:
+            a time estimate in minutes.
+        '''
+        pass
+
     def is_complete(self):
+        '''Determines if this analysis has completed successfully
+        
+        Returns:
+            True if the analysis is complete and otherwise False.
+        '''
         return self.dataSet.check_analysis_done(self)
 
     def get_analysis_name(self):
-        return self.analysisName
+        '''Get the name for this AnalysisTask.
 
-    def get_savepath(self, fileName=None):
-        if fileName is None:
-            return self.dataSet.get_analysis_subdirectory(self.analysisName)
-        else:
-            return os.sep.join([self.get_savepath(), fileName])
+        Returns:
+            the name of this AnalysisTask
+        '''
+        return self.analysisName
 
 
 class ParallelAnalysisTask(AnalysisTask):
 
-    def __init__(self, dataSet, analysisName=None):
-        super().__init__(dataSet, analysisName)
+    '''
+    An abstract class for analysis that can be run in multiple parts 
+    independently. Subclasses should implement the analysis to perform in 
+    the run_analysis() function
+    '''
+
+    def __init__(self, dataSet, parameters=None, analysisName=None):
+        super().__init__(dataSet, parameters, analysisName)
 
     @abstractmethod
     def fragment_count(self):
@@ -41,13 +102,12 @@ class ParallelAnalysisTask(AnalysisTask):
         if fragmentIndex is None:
             for i in range(self.fragment_count()):
                 self.run(i)
-        
         else:
-            self.run_for_fragment(fragmentIndex)
+            self.run_analysis(fragmentIndex)
             self.dataSet.record_analysis_complete(self, fragmentIndex) 
 
     @abstractmethod
-    def run_for_fragment(self, fragmentIndex):
+    def run_analysis(self, fragmentIndex):
         pass
 
     def is_complete(self, fragmentIndex=None):

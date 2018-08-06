57388d362f4f079d1c74a2a703617da50a6022d4
George Emanuel <emanuega0@gmail.com>
2018-08-06 11:08:48 -0400

    Added some utility functions.

--- merfish_code/core/dataset.py
+++ merfish_code/core/dataset.py
@@ -6,6 +6,7 @@ import shutil
 import pandas
 import numpy as np
 import re
+import csv
 
 from storm_analysis.sa_library import datareader
 
@@ -39,7 +40,6 @@ class DataSet(object):
         os.makedirs(self.figurePath, exist_ok=True)
 
     def save_figure(figure, figureName, subDirectory=None):
-
         if subDirectory is not None:
             savePath = os.sep.join([self.figurePath, subDirectory, figureName])
             os.makedirs(savePath, exist_ok=True)
@@ -48,6 +48,32 @@ class DataSet(object):
 
         figure.savefig(savePath + '.png', pad_inches=0)
         figure.savefig(savePath + '.pdf', transparent=True, pad_inches=0)
+
+    def _analysis_result_save_path(self, resultName, analysisName,
+            resultIndex=None, subdirectory=None):
+        saveName = resultName
+        if resultIndex is not None:
+            saveName += '_' + str(resultIndex)
+        return os.sep.join([self.get_analysis_subdirectory(
+            analysisName, subdirectory), saveName])
+
+    def save_analysis_result(self, analysisResult, resultName, 
+            analysisName, resultIndex=None, subdirectory=None):
+        #TODO - only implemented currently for ndarray
+        if not isinstance(analysisResult, np.ndarray):
+            raise TypeError('analysisResult must be a numpy array')
+
+        savePath = self._analysis_result_save_path(
+                resultName, analysisName, resultIndex, subdirectory)
+        np.save(savePath, analysisResult)
+    
+    def load_analysis_result(self, resultName, analysisName, 
+            resultIndex=None, subdirectory=None):
+        #TODO - This should determine the file extension based on the 
+        #files that are present
+        savePath = self._analysis_result_save_path(
+                resultName, analysisName, resultIndex, subdirectory) + '.npy'
+        return np.load(savePath)
 
     def get_analysis_subdirectory(self, analysisName, subdirectory=None):
         if subdirectory is None:
@@ -68,6 +94,7 @@ class DataSet(object):
         return taskDirectoryPath
         
     def save_analysis_task(self, analysisTask):
+        #TODO - this should be made more adaptable to code changes
         saveName = os.sep.join([self.get_task_subdirectory(
             analysisTask.get_analysis_name()), 'task.pkl'])
         
@@ -76,6 +103,7 @@ class DataSet(object):
                     analysisTask, outFile, protocol=pickle.HIGHEST_PROTOCOL)
 
     def load_analysis_task(self, analysisTaskName):
+        #TODO - this should be made more adaptable to code changes
         loadName = os.sep.join([self.get_task_subdirectory(
             analysisTaskName), 'task.pkl'])
 
@@ -151,12 +179,43 @@ class MERFISHDataSet(ImageDataSet):
             self._import_dataorganization(dataOrganizationName)
 
         self._load_dataorganization()
+        self._load_codebook()
         self._map_images()
 
+    def get_bit_names(self):
+        '''Get the names of the bits for this MERFISH data set.
+
+        Returns:
+            A list of the names of the bits in order from the lowest to highest
+        '''
+        return self.bitNames
+
     def get_data_channels(self):
+        '''Get the data channels for the MERFISH data set.
+
+        Returns:
+            A list of the data channels
+        '''
         return self.dataOrganization.index
 
+    def get_data_channel_for_bit(self, bitName):
+        '''Get the data channel associated with the specified bit.
+
+        Args:
+            bitName: the name of the bit to search for
+        Returns:
+            The associated data channel
+        '''
+        return self.dataOrganization[\
+                self.dataOrganization['bitName'] == bitName].index.item()
+
+
     def get_z_positions(self):
+        '''Get the z positions present in this dataset.
+
+        Returns:
+            A sorted list of all unique z positions
+        '''
         return(sorted(np.unique([x for x in self.dataOrganization['zPos']])))
 
     def get_image_path(self, imageType, fov, imagingRound):
@@ -214,12 +273,10 @@ class MERFISHDataSet(ImageDataSet):
         return self.load_image(imagePath, frame)
 
     def _map_images(self):
-        '''
-        TODO: This doesn't map the fiducial image types and currently assumes
-        that the fiducial image types and regular expressions are part of the 
-        standard image types.
-        TODO: This doesn't verify that all files are present
-        '''
+        #TODO: This doesn't map the fiducial image types and currently assumes
+        #that the fiducial image types and regular expressions are part of the 
+        #standard image types.
+        #TODO: This doesn't verify that all files are present
         mapPath = os.sep.join([self.analysisPath, 'filemap.csv'])
 
         if not os.path.exists(mapPath):
@@ -258,16 +315,28 @@ class MERFISHDataSet(ImageDataSet):
     def _parse_int_list(self, inputString):
         return self._parse_list(inputString, dtype=int)
 
+    def _parse_barcode_from_string(self, inputString):
+        return np.fromstring(inputString, dtype=int, sep=' ')
 
     def _import_codebook(self, codebookName):
-        pass
+        sourcePath = os.sep.join([os.environ.get('CODEBOOK_HOME'), \
+                codebookName + '_codebook.csv'])
+        destPath = os.sep.join([self.analysisPath, 'codebook.csv'])
 
-    def _load_dataorganization(self):
-        path = os.sep.join([self.analysisPath, 'dataorganization.csv'])
-        self.dataOrganization = pandas.read_csv(
-                path, 
-                converters={'frame': self._parse_int_list,
-                    'zPos': self._parse_list})
+        shutil.copyfile(sourcePath, destPath)    
+
+    def _load_codebook(self):
+        path = os.sep.join([self.analysisPath, 'codebook.csv'])
+
+        headerLength = 3
+        self.codebook = pandas.read_csv(
+                path, header=headerLength, skipinitialspace=True,
+                usecols=['name','id','barcode'],
+                converters={'barcode': self._parse_barcode_from_string})
+        with open(path, 'r') as inFile:
+            csvReader = csv.reader(inFile, delimiter=',')
+            header = [row for i,row in enumerate(csvReader) if i<headerLength]
+        self.bitNames = [x.strip() for x in header[2][1:]]
 
     def _convert_parameter_list(self, listIn, castFunction, delimiter=';'):
         return [castFunction(x) for x in listIn.split(delimiter) if len(x)>0]
@@ -275,12 +344,14 @@ class MERFISHDataSet(ImageDataSet):
     def _import_dataorganization(self, dataOrganizationName):
         sourcePath = os.sep.join([os.environ.get('DATA_ORGANIZATION_HOME'), \
                 dataOrganizationName + '.csv'])
-
-        if not os.path.exists(sourcePath):
-            raise FileNotFoundError(
-                    errno.ENOENT, os.strerror(errno.ENOENT), sourcePath)
-
         destPath = os.sep.join([self.analysisPath, 'dataorganization.csv'])
             
         shutil.copyfile(sourcePath, destPath)    
+
+    def _load_dataorganization(self):
+        path = os.sep.join([self.analysisPath, 'dataorganization.csv'])
+        self.dataOrganization = pandas.read_csv(
+                path, 
+                converters={'frame': self._parse_int_list,
+                    'zPos': self._parse_list})
 

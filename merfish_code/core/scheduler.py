5942b3f605d7a3d3225b64f6eb654073d8a385f6
George Emanuel <emanuega0@gmail.com>
2018-08-21 17:30:36 -0400

    Added method for calculating dependency graph.

--- merfish_code/core/scheduler.py
+++ merfish_code/core/scheduler.py
@@ -1,5 +1,6 @@
 import json
 import importlib
+import networkx
 
 class Scheduler():
 
@@ -10,16 +11,29 @@ class Scheduler():
         self._parse_parameters()
 
     def _parse_parameters(self):
-        self.analysisTasks = []
-        for tDict in parameters['analysis_tasks']:
+        self.analysisTasks = {} 
+        for tDict in self.parameters['analysis_tasks']:
             analysisModule = importlib.import_module(
                     'merfish_code.analysis.' + tDict['module'])
             analysisClass = getattr(analysisModule, tDict['task'])
             analysisParameters = tDict.get('parameters')
             analysisName = tDict.get('analysis_name')
-            self.analysisTasks.append(
-                    analysisClass(dataSet, analysisParameters, analysisName))
+            newTask = analysisClass(
+                    self.dataSet, analysisParameters, analysisName)
+            if newTask.get_analysis_name() in self.analysisTasks:
+                raise Exception('Analysis tasks must have unique names. ' + \
+                        newTask.get_analysis_name() + ' is redundant.')
+            self.analysisTasks[newTask.get_analysis_name()] = newTask
 
-
-
-
+    def _parse_dependency_graph(self):
+        graph = networkx.DiGraph()
+        roots = set()
+        for task in self.analysisTasks.values():
+            requirements = task.get_dependencies()
+            if len(requirements) == 0:
+                roots.add(task.get_analysis_name())
+            else:
+                for r in requirements:
+                    graph.add_edge(r, task.get_analysis_name())
+        return graph, roots
+        

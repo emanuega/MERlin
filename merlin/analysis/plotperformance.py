ef179df96f9c620693b496ffaf498a1d615f42ec
George Emanuel <emanuega0@gmail.com>
2018-09-05 13:08:23 -0400

    Implementation segmentation and barcode distribution plots.

--- merlin/analysis/plotperformance.py
+++ merlin/analysis/plotperformance.py
@@ -25,6 +25,8 @@ class PlotPerformance(analysistask.AnalysisTask):
                 self.parameters['decode_task'])
         self.filterTask = self.dataSet.load_analysis_task(
                 self.parameters['filter_task'])
+        self.segmentTask = self.dataSet.load_analysis_task(
+                self.parameters['segment_task'])
 
     def get_estimated_memory(self):
         return 30000
@@ -86,24 +88,84 @@ class PlotPerformance(analysistask.AnalysisTask):
         self.dataSet.save_figure(self, fig, 'barcode_intensity_area_violin')
 
     def _plot_blank_distribution(self):
-        pass
+        codebook = self.dataSet.get_codebook()
+        bc = self.filterTask.get_barcode_database().get_barcodes()
+        minX = np.min(bc['global_x'])
+        minY = np.min(bc['global_y'])
+        maxX = np.max(bc['global_x'])
+        maxY = np.max(bc['global_y'])
+
+        blankIDs = codebook[codebook['name'].str.contains('Blank')]
+        blankBC = bc[bc['barcode_id'].isin(blankIDs.index)]
+        codingIDs = codebook[~codebook['name'].str.contains('Blank')]
+
+        fig = plt.figure(figsize=(10,10))
+        ax = fig.add_subplot(111)
+        h = ax.hist2d(blankBC['global_x'], blankBC['global_y'],
+            bins=(np.ceil(maxX-minX)/5, np.ceil(maxY-minY)/5),
+            cmap=plt.get_cmap('Greys'))
+        cbar = plt.colorbar(h[3], ax=ax)
+        cbar.set_label('Spot count', rotation=270)
+        ax.set_aspect('equal', 'datalim')
+        plt.xlabel('X position (microns)')
+        plt.ylabel('Y position (microns)')
+        plt.title('Spatial distribution of blank barcodes')
+        self.dataSet.save_figure(self, fig, 'blank_spatial_distribution')
 
     def _plot_matched_barcode_distribution(self):
-        pass
+        codebook = self.dataSet.get_codebook()
+        bc = self.filterTask.get_barcode_database().get_barcodes()
+        minX = np.min(bc['global_x'])
+        minY = np.min(bc['global_y'])
+        maxX = np.max(bc['global_x'])
+        maxY = np.max(bc['global_y'])
+
+        codingIDs = codebook[~codebook['name'].str.contains('Blank')]
+        codingBC = bc[bc['barcode_id'].isin(codingIDs.index)]
+
+        fig = plt.figure(figsize=(10,10))
+        ax = fig.add_subplot(111)
+        h = ax.hist2d(codingBC['global_x'], codingBC['global_y'],
+            bins=(np.ceil(maxX-minX)/5, np.ceil(maxY-minY)/5),
+            cmap=plt.get_cmap('Greys'))
+        cbar = plt.colorbar(h[3], ax=ax)
+        cbar.set_label('Spot count', rotation=270)
+        ax.set_aspect('equal', 'datalim')
+        plt.xlabel('X position (microns)')
+        plt.ylabel('Y position (microns)')
+        plt.title('Spatial distribution of identified barcodes')
+        self.dataSet.save_figure(self, fig, 'barcode_spatial_distribution')
+
+    def _plot_cell_segmentation(self):
+        cellBoundaries = self.segmentTask.get_cell_boundaries()
+
+        fig = plt.figure(figsize=(10,10))
+        ax = fig.add_subplot(111)
+        ax.set_aspect('equal', 'datalim')
+
+        def plot_cell_boundary(boundary):
+            ax.plot([x[0] for x in boundary], [x[1] for x in boundary])
+        cellPlots = [plot_cell_boundary(b) for b in cellBoundaries]
+
+        plt.xlabel('X position (microns)')
+        plt.ylabel('Y position (microns)')
+        plt.title('Cell boundaries')
+        self.dataSet.save_figure(self, fig, 'cell_boundaries')
 
     def _plot_barcode_abundances(self):
         pass
     
-
     def run_analysis(self):
         self._plot_barcode_intensity_distribution()
         self._plot_barcode_area_distribution()
         self._plot_barcode_distance_distribution()
         self._plot_barcode_intensity_area_violin()
+        self._plot_blank_distribution()
+        self._plot_matched_barcode_distribution()
+        self._plot_cell_segmentation()
         #TODO - barcode correlation plots
         #TODO - alignment error plots - need to save transformation information
         # first
-        #TODO - good barcode and blank spatial distributions
         #TODO - barcode size spatial distribution
         #TODO - barcode distance spatial distribution
         #TODO - barcode intensity spatial distribution
@@ -111,4 +173,3 @@ class PlotPerformance(analysistask.AnalysisTask):
         #TODO - confidence ratio per barcode with blanks
         #TODO - optimization convergence
         #TODO - good barcodes/blanks per cell
-        pass

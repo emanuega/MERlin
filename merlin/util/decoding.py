755a106fbcdef5b9e695f3809012542c4b437a88
George Emanuel <emanuega0@gmail.com>
2018-09-13 13:00:45 -0400

    Updated structure of input data

--- merlin/util/decoding.py
+++ merlin/util/decoding.py
@@ -55,7 +55,7 @@ class PixelBasedDecoder(object):
         if scaleFactors is None:
             scaleFactors = self.scaleFactors
 
-        filteredImages = np.array([cv2.GaussianBlur(x, (5, 5), 1) \
+        filteredImages = np.array([cv2.GaussianBlur(x[0][0], (5, 5), 1) \
                     for x in imageData])
         #For no z data
         pixelTraces = np.reshape(

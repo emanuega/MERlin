b5c1a7fd3e72360829d4b8918d0920e133e8db5d
George Emanuel <emanuega0@gmail.com>
2018-10-24 14:50:21 -0400

    Changed the order that binary arrays are encoded into ints. Added a function for reading bits from encoded integers.

--- merlin/util/binary.py
+++ merlin/util/binary.py
@@ -2,6 +2,20 @@
 def bit_array_to_int(bitArray):
     '''Converts a binary array to an integer'''
     out = 0
-    for b in bitArray:
+    for b in reversed(bitArray):
         out = (out << 1) | b
     return out
+
+def k_bit_set(n, k):
+    '''
+    Returns true if the k'th bit of the integer n is 1, otherwise false. If
+    k is None, this function returns None.
+    '''
+    if k is None:
+        return None
+
+    if n & (1 << k):
+        return True
+    else:
+        return False
+

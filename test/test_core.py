ba329c190149c318f32430082cdafe0deb55bd95
George Emanuel <emanuega0@gmail.com>
2018-11-29 16:15:22 -0500

    Made tests of analysis tasks more thorough.

--- test/test_core.py
+++ test/test_core.py
@@ -18,21 +18,28 @@ def test_task_save(simple_data, simple_task):
     task1 = simple_task
     simple_data.save_analysis_task(task1)
     loadedTask = simple_data.load_analysis_task(task1.analysisName)
-    unsharedKeys = [k for k in task1.parameters \
+    unsharedKeys1 = [k for k in task1.parameters \
             if k not in loadedTask.parameters \
             or task1.parameters[k] != loadedTask.parameters[k]]
-    assert len(unsharedKeys)==0
+    assert len(unsharedKeys1)==0
+    unsharedKeys2 = [k for k in loadedTask.parameters \
+            if k not in task1.parameters \
+            or loadedTask.parameters[k] != task1.parameters[k]]
+    assert len(unsharedKeys2)==0
+    assert loadedTask.analysisName == task1.analysisName
 
 def test_task_run(simple_task):
     task1 = simple_task
     assert not task1.is_complete()
     assert not task1.is_running()
+    assert not task1.is_idle()
+    assert not task1.is_error()
     e = executor.LocalExecutor()
     e.run(task1, join=True)
     assert not task1.is_running()
-    assert task1.is_complete()
-    print(task1.is_error())
+    assert not task1.is_idle()
     assert not task1.is_error()
+    assert task1.is_complete()
 
 
 

a51df4227884dcea478e848282f1610b7447e985
George Emanuel <emanuega0@gmail.com>
2019-01-16 14:34:49 -0500

    Refactored between decode task and decoding utils. Updated documentation and PEP compliance.

--- merlin/analysis/decode.py
+++ merlin/analysis/decode.py
@@ -1,18 +1,16 @@
 import numpy as np
-import pandas
-from skimage import measure
+from typing import List
 
 from merlin.core import analysistask
 from merlin.util import decoding
-from merlin.util import binary
 from merlin.util import barcodedb
 
 
 class Decode(analysistask.ParallelAnalysisTask):
 
-    '''
+    """
     An analysis task that extracts barcodes from images.
-    '''
+    """
 
     def __init__(self, dataSet, parameters=None, analysisName=None):
         super().__init__(dataSet, parameters, analysisName)
@@ -36,8 +34,8 @@ class Decode(analysistask.ParallelAnalysisTask):
 
     def get_dependencies(self):
         dependencies = [self.parameters['preprocess_task'],
-                self.parameters['optimize_task'],
-                self.parameters['global_align_task']]
+                        self.parameters['optimize_task'],
+                        self.parameters['global_align_task']]
 
         if 'segment_task' in self.parameters:
             dependencies += [self.parameters['segment_task']]
@@ -45,20 +43,13 @@ class Decode(analysistask.ParallelAnalysisTask):
         return dependencies
 
     def run_analysis(self, fragmentIndex):
-        '''This function decodes the barcodes in a fov and saves them to the
+        """This function decodes the barcodes in a fov and saves them to the
         barcode database.
-        '''
+        """
         preprocessTask = self.dataSet.load_analysis_task(
                 self.parameters['preprocess_task'])
         optimizeTask = self.dataSet.load_analysis_task(
                 self.parameters['optimize_task'])
-        self.globalTask = self.dataSet.load_analysis_task(
-                self.parameters['global_align_task'])
-
-        self.segmentTask = None
-        if 'segment_task' in self.parameters:
-            self.segmentTask = self.dataSet.load_analysis_task(
-                    self.parameters['segment_task'])
 
         decoder = decoding.PixelBasedDecoder(self.dataSet.get_codebook())
         scaleFactors = optimizeTask.get_scale_factors()
@@ -71,18 +62,28 @@ class Decode(analysistask.ParallelAnalysisTask):
                     fragmentIndex, zIndex)
             imageSet = imageSet.reshape(
                 (imageSet.shape[0], imageSet.shape[-2], imageSet.shape[-1]))
+
             di, pm, npt, d = decoder.decode_pixels(imageSet, scaleFactors)
             self._extract_and_save_barcodes(
-                    di, pm, npt, d, fragmentIndex, zIndex)
+                    decoder, di, pm, npt, d, fragmentIndex, zIndex)
+
             decodedImages.append(di)
             magnitudeImages.append(pm)
 
         if self.parameters['write_decoded_images']:
+            self._save_decoded_images(
+                fragmentIndex, zPositionCount, decodedImages, magnitudeImages)
 
+    def get_barcode_database(self) -> barcodedb.BarcodeDB:
+        return barcodedb.SQLiteBarcodeDB(self.dataSet, self)
+
+    def _save_decoded_images(self, fov: int, zPositionCount: int,
+                             decodedImages: List[np.ndarray],
+                             magnitudeImages: List[np.ndarray]) -> None:
             imageDescription = self.dataSet._analysis_tiff_description(
                 zPositionCount, 2)
             with self.dataSet._writer_for_analysis_images(
-                    self, 'decoded', fragmentIndex) as outputTif:
+                    self, 'decoded', fov) as outputTif:
                 for i in range(zPositionCount):
                     outputTif.save(decodedImages[i].astype(np.float32),
                                    photometric='MINISBLACK',
@@ -94,78 +95,23 @@ class Decode(analysistask.ParallelAnalysisTask):
                                    photometric='MINISBLACK',
                                    metadata=imageDescription)
 
-    def get_barcode_database(self):
-        return barcodedb.SQLiteBarcodeDB(self.dataSet, self)
+    def _extract_and_save_barcodes(
+            self, decoder: decoding.PixelBasedDecoder, decodedImage: np.ndarray,
+            pixelMagnitudes: np.ndarray, pixelTraces: np.ndarray,
+            distances: np.ndarray, fov: int, zIndex: int) -> None:
 
-    def _initialize_barcode_dataframe(self):
-        columnInformation = self._get_bc_column_types()
-        df = pandas.DataFrame(columns=columnInformation.keys())
+        globalTask = self.dataSet.load_analysis_task(
+            self.parameters['global_align_task'])
 
-        return df
-
-    def _bc_properties_to_dict(
-            self, properties, bcIndex, fov, zIndex, distances, pixelTraces):
-        #centroid is reversed since skimage regionprops returns the centroid
-        #as (r,c)
-        centroid = properties.weighted_centroid[::-1]
-        globalCentroid = self.globalTask.fov_coordinates_to_global(
-                fov, centroid)
-        d = [distances[x[0], x[1]] for x in properties.coords]
-        zPosition = self.dataSet.z_index_to_position(zIndex)
-        outputDict = {'barcode': binary.bit_list_to_int(
-                            self.dataSet.get_codebook().get_barcode(bcIndex)), \
-                    'barcode_id': bcIndex, \
-                    'fov': fov, \
-                    'mean_intensity': properties.mean_intensity, \
-                    'max_intensity': properties.max_intensity, \
-                    'area': properties.area, \
-                    'mean_distance': np.mean(d), \
-                    'min_distance': np.min(d), \
-                    'x': centroid[0], \
-                    'y': centroid[1], \
-                    'z': zPosition, \
-                    'global_x': globalCentroid[0], \
-                    'global_y': globalCentroid[1], \
-                    'global_z': zPosition, \
-                    'cell_index': -1}
-
-        for i in range(len(pixelTraces)):
-            outputDict['intensity_' + str(i)] = \
-                np.mean([pixelTraces[i,x[0],x[1]] \
-                        for x in properties.coords])
-
-        if self.segmentTask is not None:
-            outputDict['cell_index'] = self.segmentTask \
-                    .get_cell_containing_position(
-                            globalCentroid[0], globalCentroid[1])
-
-        return outputDict
-
-    def _extract_and_save_barcodes(self, decodedImage, pixelMagnitudes, 
-            pixelTraces, distances, fov, zIndex):
+        segmentTask = None
+        if 'segment_task' in self.parameters:
+            segmentTask = self.dataSet.load_analysis_task(
+                self.parameters['segment_task'])
 
         for i in range(self.dataSet.get_codebook().get_barcode_count()):
             self.get_barcode_database().write_barcodes(
-                    self._extract_barcodes_with_index(
-                        i, decodedImage, pixelMagnitudes, pixelTraces, 
-                        distances, fov, zIndex), fov=fov)
-
-    def _position_within_crop(self, position):
-        return position[0] > self.cropWidth \
-                and position[1] > self.cropWidth \
-                and position[0] < self.imageSize[0] - self.cropWidth \
-                and position[1] < self.imageSize[1] - self.cropWidth
-
-    def _extract_barcodes_with_index(
-            self, barcodeIndex, decodedImage, pixelMagnitudes, 
-            pixelTraces, distances, fov, zIndex):
-
-        properties = measure.regionprops(
-                measure.label(decodedImage == barcodeIndex),
-                intensity_image=pixelMagnitudes)
-        dList = [self._bc_properties_to_dict(p, barcodeIndex, fov, zIndex, 
-            distances, pixelTraces) \
-                for p in properties if self._position_within_crop(p.centroid)]
-        barcodeInformation = pandas.DataFrame(dList)
-
-        return barcodeInformation
+                    decoder.extract_barcodes_with_index(
+                        i, decodedImage, pixelMagnitudes, pixelTraces,
+                        distances, fov,
+                        self.dataSet.z_index_to_position(zIndex),
+                        self.cropWidth, globalTask, segmentTask), fov=fov)
--- merlin/util/decoding.py
+++ merlin/util/decoding.py
@@ -1,13 +1,18 @@
 import numpy as np
+import pandas
 import cv2
+from typing import Tuple
+from typing import Dict
 from skimage import measure
 from sklearn.neighbors import NearestNeighbors
 
 from merlin.util import binary
+from merlin.data import codebook as mcodebook
 
-'''
+"""
 Utility functions for pixel based decoding.
-'''
+"""
+
 
 def normalize(x):
     norm = np.linalg.norm(x)
@@ -16,48 +21,72 @@ def normalize(x):
     else:
         return x
 
+
 class PixelBasedDecoder(object):
 
-    def __init__(self, codebook, scaleFactors=None):
-        self.codebook = codebook
-        self.decodingMatrix = self._calculate_normalized_barcodes()
-        self.barcodeCount = self.decodingMatrix.shape[0]
-        self.bitCount = self.decodingMatrix.shape[1]
+    def __init__(self, codebook: mcodebook.Codebook,
+                 scaleFactors: np.ndarray=None):
+        self._codebook = codebook
+        self._decodingMatrix = self._calculate_normalized_barcodes()
+        self._barcodeCount = self._decodingMatrix.shape[0]
+        self._bitCount = self._decodingMatrix.shape[1]
 
         if scaleFactors is None:
-            self.scaleFactors = np.ones(self.decodingMatrix.shape[1])
+            self._scaleFactors = np.ones(self._decodingMatrix.shape[1])
         else:
-            self.scaleFactors = scaleFactors
+            self._scaleFactors = scaleFactors
 
-    def decode_pixels(
-            self, imageData, scaleFactors=None, distanceThreshold=0.5176):
-        '''
-        Match the pixels in imagaDate to the barcodes present in the 
-        codebook. 
-        '''
+    def decode_pixels(self, imageData: np.ndarray,
+                      scaleFactors: np.ndarray=None,
+                      distanceThreshold: float=0.5176):
+        """Assign barcodes to the pixels in the provided image stock.
+
+        Each pixel is assigned to the nearest barcode from the codebook if
+        the distance between the normalized pixel trace and the barcode is
+        less than the distance threshold.
+
+        Args:
+            imageData: input image stack. The first dimension indexes the bit
+                number while the second and third dimensions contain the
+                corresponding image.
+            scaleFactors: factors to rescale each bit prior to normalization.
+                The length of scaleFactors must be equal to the number of bits.
+            distanceThreshold: the maximum distance between an assigned pixel
+                and the nearest barcode. Pixels for which the nearest barcode
+                is greater than distanceThreshold are left unassigned.
+        Returns:
+            Four results are returned as a tuple (decodedImage, pixelMagnitudes,
+                normalizedPixelTraces, distances). decodedImage is an image
+                indicating the barcode index assigned to each pixel.
+                pixelMagnitudes is an image where each pixel is the norm of
+                the pixel trace after scaling by the provided scaleFactors.
+                normalizedPixelTraces is an image stack containing the
+                normalized intensities for each pixel. distances is an
+                image containing the distance for each pixel to the assigned
+                barcode.
+        """
         if scaleFactors is None:
-            scaleFactors = self.scaleFactors
+            scaleFactors = self._scaleFactors
 
-        filteredImages = np.array([cv2.GaussianBlur(x, (5, 5), 1) \
-                    for x in imageData])
+        filteredImages = np.array([cv2.GaussianBlur(x, (5, 5), 1)
+                                   for x in imageData])
 
         pixelTraces = np.reshape(
                 filteredImages, 
                 (filteredImages.shape[0], np.prod(filteredImages.shape[1:])))
-        pixelCount = pixelTraces.shape[1]
         scaledPixelTraces = np.transpose(
-                np.array([p/s for p,s in zip(pixelTraces, scaleFactors)]))
+                np.array([p/s for p, s in zip(pixelTraces, scaleFactors)]))
         pixelMagnitudes = np.array(
                 [np.linalg.norm(x) for x in scaledPixelTraces])
-        pixelMagnitudes[pixelMagnitudes==0] = 1
-        normalizedPixelTraces = scaledPixelTraces/pixelMagnitudes[:,None]
+        pixelMagnitudes[pixelMagnitudes == 0] = 1
+        normalizedPixelTraces = scaledPixelTraces/pixelMagnitudes[:, None]
         neighbors = NearestNeighbors(n_neighbors=1)
-        neighbors.fit(self.decodingMatrix)
+        neighbors.fit(self._decodingMatrix)
         distances, indexes = neighbors.kneighbors(
                 normalizedPixelTraces, return_distance=True)
         decodedImage = np.reshape(
-            np.array([i[0] if d[0]<=distanceThreshold else -1 \
-                    for i,d in zip(indexes, distances) ]), 
+            np.array([i[0] if d[0] <= distanceThreshold else -1
+                      for i, d in zip(indexes, distances)]),
             filteredImages.shape[1:])
 
         pixelMagnitudes = np.reshape(pixelMagnitudes, filteredImages.shape[1:])
@@ -67,9 +96,100 @@ class PixelBasedDecoder(object):
         distances = np.reshape(distances, filteredImages.shape[1:])
         return decodedImage, pixelMagnitudes, normalizedPixelTraces, distances
 
+    # TODO  barcodes here has two different meanings. One of these should be
+    # renamed.
+    def extract_barcodes_with_index(
+            self, barcodeIndex: int, decodedImage: np.ndarray,
+            pixelMagnitudes: np.ndarray, pixelTraces: np.ndarray,
+            distances: np.ndarray, fov: int, zPosition: float,
+            cropWidth: float, globalAligner,
+            segmenter=None) -> pandas.DataFrame:
+        """Extract the barcode information from the decoded image for barcodes
+        that were decoded to the specified barcode index.
+
+        Args:
+            barcodeIndex: the index of the barcode to extract the corresponding
+                barcodes
+            decodedImage: the image indicating the barcode index assigned to
+                each pixel
+            pixelMagnitudes: an image containing norm of the intensities for
+                each pixel across all bits after scaling by the scale factors
+            pixelTraces: an image stack containing the normalized pixel
+                intensity traces
+            distances: an image indicating the distance between the normalized
+                pixel trace and the assigned barcode for each pixel
+            fov: the index of the field of view
+            zPosition: the z position
+            cropWidth: the number of pixels around the edge of each image within
+                which barcodes are excluded from the output list.
+            globalAligner: the aligner used for converted to local x,y
+                coordinates to global x,y coordinates
+            segmenter: the cell segmenter for assigning a cell for each of the
+                identified barcodes
+        Returns:
+            a pandas dataframe containing all the barcodes decoded with the
+                specified barcode index
+        """
+        properties = measure.regionprops(
+                measure.label(decodedImage == barcodeIndex),
+                intensity_image=pixelMagnitudes)
+        dList = [self._bc_properties_to_dict(p, barcodeIndex, fov, zPosition,
+                                             distances, pixelTraces,
+                                             globalAligner, segmenter)
+                 for p in properties if self._position_within_crop(
+                    p.centroid, cropWidth, decodedImage.shape)]
+        barcodeInformation = pandas.DataFrame(dList)
+
+        return barcodeInformation
+
+    @staticmethod
+    def _position_within_crop(position: np.ndarray, cropWidth: float,
+                              imageSize: Tuple[int]) -> bool:
+        return cropWidth < position[0] < imageSize[0] - cropWidth \
+                and cropWidth < position[1] < imageSize[1] - cropWidth
+
+    def _bc_properties_to_dict(self, properties, bcIndex: int, fov: int,
+                               zPosition: float, distances: np.ndarray,
+                               pixelTraces: np.ndarray, globalAligner,
+                               segmenter=None) -> Dict:
+        # centroid is reversed since skimage regionprops returns the centroid
+        # as (r,c)
+        centroid = properties.weighted_centroid[::-1]
+        globalCentroid = globalAligner.fov_coordinates_to_global(
+                fov, centroid)
+        d = [distances[x[0], x[1]] for x in properties.coords]
+        outputDict = {'barcode': binary.bit_list_to_int(
+                            self._codebook.get_barcode(bcIndex)),
+                      'barcode_id': bcIndex,
+                      'fov': fov,
+                      'mean_intensity': properties.mean_intensity,
+                      'max_intensity': properties.max_intensity,
+                      'area': properties.area,
+                      'mean_distance': np.mean(d),
+                      'min_distance': np.min(d),
+                      'x': centroid[0],
+                      'y': centroid[1],
+                      'z': zPosition,
+                      'global_x': globalCentroid[0],
+                      'global_y': globalCentroid[1],
+                      'global_z': zPosition,
+                      'cell_index': -1}
+
+        for i in range(len(pixelTraces)):
+            outputDict['intensity_' + str(i)] = \
+                np.mean([pixelTraces[i, x[0], x[1]]
+                        for x in properties.coords])
+
+        if segmenter is not None:
+            outputDict['cell_index'] = segmenter \
+                    .get_cell_containing_position(
+                            globalCentroid[0], globalCentroid[1])
+
+        return outputDict
+
     def _calculate_normalized_barcodes(
             self, ignoreBlanks=False, includeErrors=False):
-        '''Normalize the barcodes present in the provided codebook so that 
+        """Normalize the barcodes present in the provided codebook so that
         their L2 norm is 1.
 
         Args:
@@ -80,47 +200,64 @@ class PixelBasedDecoder(object):
                 errors should be added.
         Returns:
             A 2d numpy array where each row is a normalized barcode and each
-            column is the corresponding normalized bit value.
-        '''
+                column is the corresponding normalized bit value.
+        """
         
-        barcodeSet = self.codebook.get_barcodes(ignoreBlanks=ignoreBlanks)
+        barcodeSet = self._codebook.get_barcodes(ignoreBlanks=ignoreBlanks)
         magnitudes = np.sqrt(np.sum(barcodeSet*barcodeSet, axis=1))
        
         if not includeErrors:
             weightedBarcodes = np.array(
-                [normalize(x) for x,m in zip(barcodeSet, magnitudes)])
+                [normalize(x) for x, m in zip(barcodeSet, magnitudes)])
             return weightedBarcodes
 
         else:
             barcodesWithSingleErrors = []
             for b in barcodeSet:
-                barcodeSet = np.array([b] \
-                        + [binary.flip_bit(b, i) for i in range(len(b))])
+                barcodeSet = np.array([b]
+                                      + [binary.flip_bit(b, i)
+                                         for i in range(len(b))])
                 bcMagnitudes = np.sqrt(np.sum(barcodeSet*barcodeSet, axis=1))
                 weightedBC = np.array(
-                    [x/m for x,m in zip(barcodeSet, bcMagnitudes)])
+                    [x/m for x, m in zip(barcodeSet, bcMagnitudes)])
                 barcodesWithSingleErrors.append(weightedBC)
             return np.array(barcodesWithSingleErrors)
             
-    def extract_refactors(self, imageSet):
+    def extract_refactors(
+            self, imageSet: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        """Calculate the scale factors that would result in the mean
+        on bit intensity for each bit to be equal to one.
+
+        If the scale factors for this decoder are not set to 1, then the
+        calculated scale factors are dependent on the input scale factors
+        used for the decoding.
+
+        Args:
+            imageSet: the image stack to decode in order to determine the
+                scale factors
+
+        Returns:
+            a tupble containing an a array of the scale factors where the i'th
+                entry is the scale factor for bit i and an array indicating
+                the abundance of each barcode determined during the decoding
+        """
         di, pm, npt, d = self.decode_pixels(imageSet)
 
-        sumPixelTraces = np.zeros((self.barcodeCount, self.bitCount))
-        barcodesSeen = np.zeros(self.barcodeCount)
-        for b in range(self.barcodeCount):
-            barcodeRegions = [x \
-                    for x in measure.regionprops(
-                        measure.label((di==b).astype(np.int))) if x.area >= 4]
+        sumPixelTraces = np.zeros((self._barcodeCount, self._bitCount))
+        barcodesSeen = np.zeros(self._barcodeCount)
+        for b in range(self._barcodeCount):
+            barcodeRegions = [x for x in measure.regionprops(
+                        measure.label((di == b).astype(np.int))) if x.area >= 4]
             barcodesSeen[b] = len(barcodeRegions)
             for br in barcodeRegions:
                 meanPixelTrace = \
-                    np.mean([npt[:, y[0], y[1]]*pm[y[0],y[1]] \
-                    for y in br.coords], axis=0)
+                    np.mean([npt[:, y[0], y[1]]*pm[y[0], y[1]]
+                             for y in br.coords], axis=0)
                 normPixelTrace = meanPixelTrace/np.linalg.norm(meanPixelTrace)
-                sumPixelTraces[b,:] += normPixelTrace/barcodesSeen[b]
+                sumPixelTraces[b, :] += normPixelTrace/barcodesSeen[b]
 
-        sumPixelTraces[self.decodingMatrix == 0] = np.nan
+        sumPixelTraces[self._decodingMatrix == 0] = np.nan
         onBitIntensity = np.nanmean(sumPixelTraces, axis=0)
         refactors = onBitIntensity/np.mean(onBitIntensity)
-        return refactors, barcodesSeen
 
+        return refactors, barcodesSeen

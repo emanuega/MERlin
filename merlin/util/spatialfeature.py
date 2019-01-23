02d5f30e205e569456f13a75f656256d179d5b06
George Emanuel <emanuega0@gmail.com>
2019-01-23 14:35:14 -0500

    Began implementing spatial feature.

--- merlin/util/spatialfeature.py
+++ merlin/util/spatialfeature.py
@@ -1,55 +1,115 @@
 import numpy as np
+import uuid
+import cv2
+from typing import List
+from shapely import geometry
 
 
-class VolumeFeature(object):
+class SpatialFeature(object):
 
     """
-    A feature is a collection of contiguous voxels.
+    A spatial feature is a collection of contiguous voxels.
     """
 
-    def __init__(self, pixelList: np.ndarray, fov: int) -> None:
+    def __init__(self, boundaryList: List[List[geometry.Polygon]], fov: int,
+                 uniqueID=None) -> None:
         """Create a new feature specified by a list of pixels
 
         Args:
-            pixelList: a list of pixels that are included in this feature.
-                Each pixel is specified by x, y, and z integers.
+            boundaryList: a least of boundaries that define this feature.
+                The first index of the list corresponds with the z index.
+                The second index corresponds with the index of the shape since
+                some regions might split in some z indexes.
             fov: the index of the field of view that this feature belongs to.
                 The pixel list specifies pixel in the local fov reference
                 frame.
         """
-
-        self._pixelList = pixelList.copy().astype(np.uint32)
+        self._boundaryList = boundaryList
         self._fov = fov
 
-    def get_fov(self):
+        if uniqueID is None:
+            self._uniqueID = uuid.uuid4()
+        else:
+            self._uniqueID = uniqueID
+
+    @staticmethod
+    def feature_from_label_matrix(labelMatrix: np.ndarray, fov: int,
+                                  transformationMatrix: np.ndarray=None):
+        """Generate a new feature from the specified label matrix.
+
+        Args:
+            labelMatrix: a 3d matrix indicating the z, x, y position
+                of voxels that contain the feature. Voxels corresponding
+                to the feature have a value of True while voxels outside of the
+                feature should have a value of False.
+            fov: the index of the field of view corresponding to the
+                label matrix.
+            transformationMatrix: a 3x3 numpy array specifying the
+                transformation from fov to global coordinates. If None,
+                the feature coordinates are not transformed.
+
+        Returns: the new feature
+        """
+
+        boundaries = [SpatialFeature._extract_boundaries(x)
+                      for x in labelMatrix]
+
+        if transformationMatrix is not None:
+            boundaries = [SpatialFeature._transform_boundaries(
+                x, transformationMatrix) for x in boundaries]
+
+        return SpatialFeature([[geometry.Polygon(x) for x in b if len(x) > 2]
+                               for b in boundaries], fov)
+
+    @staticmethod
+    def _extract_boundaries(labelMatrix: np.ndarray) -> List[np.ndarray]:
+        """Determine the boundaries of the feature indicated in the
+        label matrix.
+
+        Args:
+            labelMatrix: a 2 dimensional numpy array indicating the x, y
+                position of pixels that contain the feature.
+        Returns: a list of n x 2 numpy arrays indicating the x, y coordinates
+            of the boundaries where n is the number of boundary coordinates
+        """
+        _, boundaries, _ = cv2.findContours(
+            labelMatrix.copy().astype(np.uint8), cv2.RETR_EXTERNAL,
+            cv2.CHAIN_APPROX_NONE)
+
+        print(boundaries)
+        return [np.array([[x[0][0], x[0][1]] for x in y]) for y in boundaries]
+
+    @staticmethod
+    def _transform_boundaries(
+            boundaries: List[np.ndarray],
+            transformationMatrix: np.ndarray) -> List[np.ndarray]:
+
+        transformedList = []
+        for b in boundaries:
+            reshapedBoundaries = np.reshape(
+                b, (1, b.shape[0], 2)).astype(np.float)
+            transformedBoundaries = cv2.transform(
+                reshapedBoundaries, transformationMatrix)[0, :, :2]
+            transformedList.append(transformedBoundaries)
+
+        return transformedList
+
+    def get_fov(self) -> int:
         return self._fov
 
-    def get_pixels(self):
-        return self._pixelList
+    def get_boundaries(self) -> List[List[geometry.Polygon]]:
+        return self._boundaryList
 
-    def overlaps_in_fov(self, inFeature) -> bool:
-        """Determine if this feature overlaps with the specified feature.
-
-        This function only checks for overlap between features associated
-        with the same field of view. It is possible that two features
-        from different field of views overlap because of their global
-        arrangement, but this function will not detect that overlap.
+    def is_contained_in(self, inFeature) -> bool:
+        """Determine if the boundary of this feature is contained in the
+        boundary of the specified feature.
 
         Args:
             inFeature: the feature to check for overlap with
         Returns:
-            True if this feature and inFeature are in the same field of view
-                and contains pixels that are also in inFeature,
-                otherwise False.
+            True if inFeature contains pixels that are within inFeature,
+                otherwise False. This returns false if inFeature only shares
+                a boundary with this feature.
         """
-        if self.get_fov() != inFeature.get_fov():
-            return False
-
-        for p1 in self.get_pixels():
-            for p2 in inFeature.get_pixels():
-                if np.array_equal(p1, p2):
-                    return True
-
-        return False
-
+        raise NotImplementedError
 
---
+++ test/test_spatialfeature.py
@@ -0,0 +1,33 @@
+import numpy as np
+from shapely import geometry
+
+from merlin.util import spatialfeature
+
+
+def test_feature_from_label_matrix():
+    testLabels = np.zeros((1, 4, 4))
+    testLabels[0, 1:3, 1:3] = 1
+
+    feature = spatialfeature.SpatialFeature.feature_from_label_matrix(
+        testLabels, 0)
+
+    assert len(feature.get_boundaries()[0]) == 1
+    assert feature.get_boundaries()[0][0].equals(geometry.Polygon(
+        [[1, 1], [1, 2], [2, 2], [2, 1], [1, 1]]
+    ))
+
+
+def test_feature_from_label_matrix_transform():
+    testLabels = np.zeros((1, 4, 4))
+    testLabels[0, 1:3, 1:3] = 1
+    transformMatrix = np.array([[2.0, 0, 3],
+                                [0, 2.5, 1],
+                                [0, 0, 1]])
+
+    feature = spatialfeature.SpatialFeature.feature_from_label_matrix(
+        testLabels, 0, transformMatrix)
+
+    assert len(feature.get_boundaries()[0]) == 1
+    assert feature.get_boundaries()[0][0].equals(geometry.Polygon(
+        [[5, 3.5], [5, 6], [7, 6], [7, 3.5], [5, 3.5]]
+    ))

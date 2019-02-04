2cec282b5aa71a7c32fdedfc92bd3da95bbb4f9e
George Emanuel <emanuega0@gmail.com>
2019-02-04 12:50:31 -0500

    Updated executor to more easily restart analysis after an error.

--- merlin/core/executor.py
+++ merlin/core/executor.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 import multiprocessing
 import threading
+from typing import Callable
 
 from merlin.core import analysistask
 
@@ -11,8 +12,30 @@ class Executor(object):
         super().__init__()
     
     @abstractmethod
-    def run(self):
-        pass 
+    def run(self, task: analysistask.AnalysisTask, index: int=None,
+            callback: Callable=None, join: bool=False,
+            rerunCompleted: bool=False) -> None:
+        """Run an analysis task.
+
+        This method will not run analysis tasks that are already currently
+        running and analysis is terminated early due to error or otherwise
+        will not be restarted.
+
+        Args:
+            task: the analysis task to run.
+            index: index of the analysis to run for a parallel analysis task.
+            callback: function ta call upon completion of the analysis.
+            join: flag indicating if the execution threads should be joined to
+                the current thread. If True, this function doesn't return
+                until the analysis is complete.
+            rerunCompleted: flag indicating if previous analysis should be
+                run again even if it has previously completed. If overwrite
+                is True, analysis will be run on the task regardless of its
+                status. If overwrite is False, analysis will only be run on
+                the task or fragments of the task that have either not been
+                started or have previously completed in error.
+        """
+        pass
 
 
 class LocalExecutor(Executor):
@@ -25,25 +48,33 @@ class LocalExecutor(Executor):
         else:
             self.coreCount = coreCount
 
-    def run(self, task, index=None, callback=None, join=False):
+    def run(self, task: analysistask.AnalysisTask, index: int=None,
+            callback: Callable=None, join: bool=False,
+            rerunCompleted: bool=False) -> None:
+        if task.is_complete() and not rerunCompleted:
+            return
+
         pool = None
         thread = None
         if isinstance(task, analysistask.ParallelAnalysisTask):
             pool = multiprocessing.Pool(processes=self.coreCount)
-            pool.map_async(
-                    task.run, range(task.fragment_count()),
-                    callback=callback)
-        elif isinstance(task, analysistask.InternallyParallelAnalysisTask) \
-                and index is None:
+            fragmentsToRun = [x for x in range(task.fragment_count())
+                              if (rerunCompleted and not task.is_running(x))
+                              or (not task.is_complete(x)
+                                  and not task.is_running(x))]
+            pool.map_async(task.run, fragmentsToRun, callback=callback)
+
+        elif isinstance(task, analysistask.InternallyParallelAnalysisTask):
+            if task.is_running():
+                return
             task.set_core_count(self.coreCount)
             thread = threading.Thread(target=task.run)
             thread.daemon = True
             thread.start()
-        elif isinstance(task, analysistask.InternallyParallelAnalysisTask) \
-                and index is not None:
-            pool = multiprocessing.Pool(processes=1)
-            pool.apply_async(task.run, index, callback=callback)
+
         else:
+            if task.is_running():
+                return
             pool = multiprocessing.Pool(processes=1)
             pool.apply_async(task.run, callback=callback)
 

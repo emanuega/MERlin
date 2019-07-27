2b20eed613028fd56accc49aebe5cf05a6d532a0
emanuega <emanuega0@gmail.com>
2019-07-27 02:41:05 -0400

    Clarified error message when raw data path isn't found.

--- merlin/core/dataset.py
+++ merlin/core/dataset.py
@@ -58,10 +58,9 @@ class DataSet(object):
 
         self.rawDataPath = os.sep.join([dataHome, dataDirectoryName])
         if not os.path.isdir(self.rawDataPath):
-            print('Cannot find {}'.format(self.rawDataPath))
-            # raise FileNotFoundError(
-            #         errno.ENOENT, os.strerror(errno.ENOENT), self.rawDataPath)
-            
+            print('Unable to find raw data path at {}.'
+                  .format(self.rawDataPath))
+
         self.analysisPath = os.sep.join([analysisHome, dataDirectoryName])
         os.makedirs(self.analysisPath, exist_ok=True)
 

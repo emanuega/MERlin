c41027ba478d4dcf9678358b377903933f72270b
George Emanuel <emanuega0@gmail.com>
2019-08-08 10:27:59 -0400

    Updated snakewriter to only generate rules for analysis tasks that are not complete.

--- merlin/util/snakewriter.py
+++ merlin/util/snakewriter.py
@@ -130,7 +130,8 @@ class SnakefileGenerator(object):
             for d in a.get_dependencies():
                 taskGraph.add_edge(d, x)
 
-        return [k for k, v in taskGraph.out_degree if v == 0]
+        return [k for k, v in taskGraph.out_degree if v == 0
+                and not analysisTasks[k].is_complete()]
 
     def generate_workflow(self) -> str:
         """Generate a snakemake workflow for the analysis parameters
@@ -141,7 +142,7 @@ class SnakefileGenerator(object):
         """
         analysisTasks = self._parse_parameters()
         ruleList = {k: SnakemakeRule(v, self._pythonPath)
-                    for k, v in analysisTasks.items()}
+                    for k, v in analysisTasks.items() if not v.is_complete()}
 
         terminalTasks = self._identify_terminal_tasks(analysisTasks)
         workflowString = 'rule all: \n\tinput: ' + \

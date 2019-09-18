c38859d73c64aac6fcee7078ecd89b1ebba8b058
George Emanuel <emanuega0@gmail.com>
2019-09-18 00:29:00 -0400

    Updated dataset to write a json file with metadata for reloading the dataset.

--- merlin/core/dataset.py
+++ merlin/core/dataset.py
@@ -3,7 +3,6 @@ import json
 import shutil
 import pandas
 import numpy as np
-import fnmatch
 import tifffile
 import importlib
 import time
@@ -67,6 +66,15 @@ class DataSet(object):
 
         self.logPath = os.sep.join([self.analysisPath, 'logs'])
         os.makedirs(self.logPath, exist_ok=True)
+
+    def _store_dataset_metadata(self) -> None:
+        metadata = {
+            'merlin_version': merlin.version(),
+            'module': type(self).__module__,
+            'class': type(self).__name__,
+            'dataset_name': self.dataSetName
+        }
+        self.save_json_analysis_result(metadata, 'dataset')
 
     def save_workflow(self, workflowString: str) -> str:
         """ Save a snakemake workflow for analysis of this dataset.
@@ -931,7 +939,6 @@ class MERFISHDataSet(ImageDataSet):
                     file that specifies properties of the microscope used
                     to acquire the images represented by this ImageDataSet
         """
-
         super().__init__(dataDirectoryName, dataHome, analysisHome,
                          microscopeParametersName)
 

20b359194e4bace4f9e067229444382863bc5861
George Emanuel <emanuega0@gmail.com>
2019-09-18 00:56:38 -0400

    Fixed format of blank fov distribution plot.
    Added plot for scale factor magnitude.
    
    Fixed saving dataset metadata.
    
    Fixed the way color channels are determined.
    
    Fixed indexing for scale factors by color.
    
    Fixed indexing for scale factors by color.
    
    Fixed iteration of scale factors by color.
    
    Set lower y limit of scale factor plot to 0.

--- merlin/plots/optimizationplots.py
+++ merlin/plots/optimizationplots.py
@@ -25,6 +25,42 @@ class OptimizationScaleFactorsPlot(AbstractPlot):
         return fig
 
 
+class ScaleFactorVsBitNumberPlot(AbstractPlot):
+
+    def __init__(self, analysisTask):
+        super().__init__(analysisTask)
+
+    def get_required_tasks(self):
+        return {'optimize_task': 'all'}
+
+    def get_required_metadata(self):
+        return []
+
+    def _generate_plot(self, inputTasks, inputMetadata):
+        optimizeTask = inputTasks['optimize_task']
+        codebook = optimizeTask.get_codebook()
+        dataOrganization = optimizeTask.dataSet.get_data_organization()
+        colors = [dataOrganization.get_data_channel_color(
+            dataOrganization.get_data_channel_for_bit(x))
+            for x in codebook.get_bit_names()]
+
+        scaleFactors = optimizeTask.get_scale_factors()
+        scaleFactorsByColor = {c: [] for c in set(colors)}
+        for i, s in enumerate(scaleFactors):
+            scaleFactorsByColor[colors[i]].append((i, s))
+
+        fig = plt.figure(figsize=(5, 5))
+        for c, d in scaleFactorsByColor.items():
+            plt.plot([x[0] for x in d], [x[1] for x in d], 'o')
+
+        plt.legend(scaleFactorsByColor.keys())
+        plt.ylim(bottom=0)
+        plt.xlabel('Bit index')
+        plt.ylabel('Scale factor magnitude')
+        plt.title('Scale factor magnitude vs bit index')
+        return fig
+
+
 class OptimizationBarcodeCountsPlot(AbstractPlot):
 
     def __init__(self, analysisTask):

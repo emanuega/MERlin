34e4d097c2b3a7e42f65d526bbd753021636f57e
emanuega <emanuega0@gmail.com>
2019-09-25 21:50:46 -0400

    Incremented version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import setuptools
 
 CLASSIFIERS = [
-    "Development Status :: 2 - Pre-Alpha",
+    "Development Status :: 4 - Beta",
     "Natural Language :: English",
     "Operating System :: POSIX",
     "Operating System :: Unix",
@@ -17,7 +17,7 @@ install_requires = [line.rstrip() for line in open(
 
 setuptools.setup(
     name="merlin",
-    version="0.0.1",
+    version="0.1.0",
     description="MERFISH decoding software",
     author="George Emanuel",
     author_email="emanuega0@gmail.com",
@@ -25,8 +25,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     entry_points={
-        'console_scripts': ["merlin=merlin.merlin:merlin",
-                            "merlinview=merlin.view.merlinview:merlin_view"]
+        'console_scripts': ["merlin=merlin.merlin:merlin"]
     },
     classifiers=CLASSIFIERS
 )

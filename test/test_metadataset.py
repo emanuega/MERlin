56c60c47436d9ef42377380ce02d300dc235abb2
Stephen Eichhorn <stephen_eichhorn@fas.harvard.edu>
2019-10-17 23:05:49 -0400

    downgrading matplotlib

--- test/test_metadataset.py
+++ test/test_metadataset.py
@@ -1,5 +1,4 @@
 def test_construct_metadataset(simple_metamerfish_data):
     assert simple_metamerfish_data.metaDataSetParameters[
                'overwrite_cached'] == False
-    assert ('metamerfish_test_dataset' in
-            simple_metamerfish_data.dataSetDict) == 1
+    assert ('merfish_test' in simple_metamerfish_data.dataSetDict) == 1

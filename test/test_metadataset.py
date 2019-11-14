f221f8defe721b1d1018907e6da548f41c557fdc
Stephen Eichhorn <stephen_eichhorn@fas.harvard.edu>
2019-11-14 11:52:57 -0500

    debugging tests

--- test/test_metadataset.py
+++ test/test_metadataset.py
@@ -1,4 +1,2 @@
 def test_construct_metadataset(simple_metamerfish_data):
-    assert simple_metamerfish_data.metaDataSetParameters[
-               'overwrite_cached'] == 0
-    assert ('merfish_test' in simple_metamerfish_data.dataSetDict) == 1
+    assert ('merfish_test' in simple_metamerfish_data.dataSets) == 1

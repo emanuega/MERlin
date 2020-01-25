05a9b7a8c581af2cd7b5f3dcb815db312cdc9ee1
leonardosepulveda <leonardosepulveda@gmail.com>
2020-01-25 11:13:39 -0500

    pep8 compliance

--- merlin/analysis/segment.py
+++ merlin/analysis/segment.py
@@ -225,7 +225,7 @@ class WatershedSegmentNucleiCV2(FeatureSavingAnalysisTask):
             self.parameters['warp_task'])
 
         imageStack = np.array([warpTask.get_aligned_image(fov, channelIndex, z)
-                                for z in range(len(self.dataSet.
+                               for z in range(len(self.dataSet.
                                                     get_z_positions()))])
         # generate mask based on edge detection
         """

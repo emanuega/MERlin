ec91b780c32659b2659c88e497ef7168b1de2dc7
leonardosepulveda <leonardosepulveda@gmail.com>
2020-04-23 22:16:23 -0400

    add body of apply_machine_learning_segmentation function

--- merlin/util/machinelearningsegmentation.py
+++ merlin/util/machinelearningsegmentation.py
@@ -13,14 +13,33 @@ from merlin.util import matlab
 """
 This module contains utility functions for preparing imagmes for performing
 segmentation using machine learning approaches
+MAYBE COMBINE WITH WATERSHED.PY INTO A SINGLE FILE, SEGMENTATION.PY
 """
-
-def apply_machine_learning_segmentation(imageStackIn: np.ndarray,
-										method: str) -> np.ndarray:
-
 def segment_using_ilastik(imageStackIn: np.ndarray) -> np.ndarray:
 
 def segment_using_unet(imageStackIn: np.ndarray) -> np.ndarray:
 
 def segment_using_cellpose(imageStackIn: np.ndarray) -> np.ndarray:
+
+
+def apply_machine_learning_segmentation(imageStackIn: np.ndarray,
+										method: str) -> np.ndarray:
+	"""Calculate binary mask with 1's in membrane pixels and 0 otherwise.
+    The images expected are some type of Nuclei label (e.g. DAPI)
+
+    Args:
+        membraneImages: a 3 dimensional numpy array containing the images
+            arranged as (z, x, y).
+    Returns:
+        ndarray containing a 3 dimensional mask arranged as (z, x, y)
+    """
+	if method == 'ilastik':
+		segmentOutput = segment_using_ilastik(imageStackIn)
+	elif method == 'cellpose':
+		segmentOutput = segment_using_cellpose(imageStackIn)
+	elif method == 'unet'
+		segmentOutput = segment_using_unet(imageStackIn)
+
+	return segmentOutput
+
 

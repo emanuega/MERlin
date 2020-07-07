323e8809fc85a114a3ff2d3c17f87bcc62e3ad66
leonardosepulveda <leonardosepulveda@gmail.com>
2020-07-07 18:52:17 -0400

    add test for new segmentation routines

---
+++ test/test_segmentation_cv2.py
@@ -0,0 +1,15 @@
+import os
+import pytest
+
+import merlin
+from merlin import merlin as m
+
+
+@pytest.mark.fullrun
+@pytest.mark.slowtest
+def test_merfish_2d_full_local(simple_merfish_data):
+    with open(os.sep.join([merlin.ANALYSIS_PARAMETERS_HOME,
+                           'test_analysis_segmentation_cv2.json']), 'r') as f:
+        snakefilePath = m.generate_analysis_tasks_and_snakefile(
+            simple_merfish_data, f)
+        m.run_with_snakemake(simple_merfish_data, snakefilePath, 5)

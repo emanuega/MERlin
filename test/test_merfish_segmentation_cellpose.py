5bbd4f680cee4c2c0ed6fba9a096b0445ba712b3
leonardosepulveda <leonardosepulveda@gmail.com>
2020-07-09 14:46:34 -0400

    change json name in test py file

--- test/test_merfish_segmentation_cellpose.py
+++ test/test_merfish_segmentation_cellpose.py
@@ -9,7 +9,7 @@ from merlin import merlin as m
 @pytest.mark.slowtest
 def test_cellpose_2d_local(simple_merfish_data):
     with open(os.sep.join([merlin.ANALYSIS_PARAMETERS_HOME,
-                           'test_analysis_parameters_segmentation_cellpose.json']), 'r') as f:
+                           'test_analysis_segmentation_cellpose.json']), 'r') as f:
         snakefilePath = m.generate_analysis_tasks_and_snakefile(
             simple_merfish_data, f)
         m.run_with_snakemake(simple_merfish_data, snakefilePath, 5)

acc56bac24d02f78b5ff865987918336488cf98a
George Emanuel <emanuega0@gmail.com>
2020-09-22 13:53:01 -0400

    Fixed problem caused by fileHandle not being set when path doesn't exist

--- merlin/util/dataportal.py
+++ merlin/util/dataportal.py
@@ -256,6 +256,7 @@ class LocalFilePortal(FilePortal):
 
     def __init__(self, fileName: str):
         super().__init__(fileName)
+        self._fileHandle = None
         if not os.path.exists(fileName):
             raise FileNotFoundError
         self._fileHandle = open(fileName, 'rb')
@@ -275,7 +276,8 @@ class LocalFilePortal(FilePortal):
         return self._fileHandle.read(endByte-startByte)
 
     def close(self) -> None:
-        self._fileHandle.close()
+        if self._fileHandle is not None:
+            self._fileHandle.close()
 
 
 class S3FilePortal(FilePortal):
